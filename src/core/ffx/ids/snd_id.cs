843b48fdd00537543fb359342ed5ab24f3b0e64a
Dawn Veilwinter <DawnVeilwinter@protonmail.com>
2026-09-14 17:51:47 +0200

    Correct 'SoundId.PAGE_TURN'

--- src/core/ffx/ids/snd_id.cs
+++ src/core/ffx/ids/snd_id.cs
@@ -8,5 +8,5 @@ public class SoundId {
     public const uint UI_CONFIRM  = 0x80000002;
     public const uint UI_DISABLED = 0x80000003;
     public const uint UI_CANCEL   = 0x80000004;
-    public const uint PAGE_TURN   = 0x80000048;
+    public const uint PAGE_TURN   = 0x80000030;
 }
