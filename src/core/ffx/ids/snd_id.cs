404b5cb4cf7cd8ba74e57d6cbc0bfbb1f69937c2
Dawn Veilwinter <DawnVeilwinter@protonmail.com>
2026-09-14 17:29:30 +0200

    Fix up Sound IDs
    I remembered wrong

--- src/core/ffx/ids/snd_id.cs
+++ src/core/ffx/ids/snd_id.cs
@@ -4,7 +4,8 @@
 // It is licensed to you under the GNU Lesser General Public License, version 3.0 or later. See COPYING, COPYING.LESSER.
 
 public class SoundId {
-    public const uint UI_MOVE = 0x80000001;
-    public const uint UI_CANCEL = 0x80000003;
-    public const uint UI_CONFIRM = 0x80000004;
+    public const uint UI_MOVE     = 0x80000001;
+    public const uint UI_CONFIRM  = 0x80000002;
+    public const uint UI_DISABLED = 0x80000003;
+    public const uint UI_CANCEL   = 0x80000004;
 }
